30d24351e2c28a9a595c71ad0216a94d1a5fe694
ilahajiyeva <44476352+ilahajiyeva@users.noreply.github.com>
2021-01-08 13:30:13 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var names = ['Paul', 'Frank', 'Larry', 'Paula'];
+
+for (var i = 0; i < names.length; i++) {
+    console.log("Hello " + names[i]);
+}
+
+
+var person = "Good Bye";
+
+function goodbye(name) {
+    console.log(person + " " + name);
+}
+
+goodbye("Jim");
